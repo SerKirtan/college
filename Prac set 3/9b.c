dd1a62b59f3f3ca8eb88e2c2d571e3df7002b542
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-23 10:11:49 +0530

    Update 9b.c

--- Prac set 3/9b.c
+++ Prac set 3/9b.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main(){
 
-    int r,c,x;
+    int r,c,x;//replace;
     int rows=5;
-    for(c=1;c<=rows;c++)
+    /*replace = c;
+    c=r;
+    r=replace;*/
+    for(r=1;r<=rows;r++)
     {
-        for(r=1,x=1;r<=2*rows-1;r++)
+        for(c=1,x=1;c<=2*rows-1;c++)
         {
-            if(r>=rows-(c-1)&&r<=rows+(c-1))
+            if(c>=rows-(r-1)&&c<=rows+(r-1))
             {
                 if(r%2==0 && c%2==1 || r%2==1 && c%2==0)
                     printf(" ");
