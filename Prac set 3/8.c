58344c667e13dcedc2f1d8e68c5e46ad98617791
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 22:52:00 +0530

    Update 8.c

--- Prac set 3/8.c
+++ Prac set 3/8.c
@@ -1,1 +1,18 @@
 //Sum of 1/1 + 1/2 + 1/3 + ... + 1/n
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    float t=1,sum=0,i;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        t=1/i;
+        sum=sum+t;
+    }
+    printf("Sum upto %d terms is %.3f",n,sum);
+    return 0;
+}
