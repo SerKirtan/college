65502fe22122805069bf2c51ef72b6865ba8a1e5
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:15:49 +0530

    Create 3a.c

---
+++ Prac set 2/3a.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 3 numbers\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a>b)
+    {
+        if (a>c)
+            printf("%d is maximum\n",a);
+        else
+            printf("%d is maximum\n",c);
+    }
+    else
+    {
+        if (b>c)
+            printf("%d is maximum\n",b);
+        else
+            printf("%d is maximum\n",c);
+    }
+    if (a<b)
+    {
+        if (a<c)
+            printf("%d is minimum",a);
+        else
+            printf("%d is minimum",c);
+    }
+    else
+    {
+        if (b<c)
+            printf("%d is minimum",b);
+        else
+            printf("%d is minimum",c);
+    }
+    return 0;
+}
