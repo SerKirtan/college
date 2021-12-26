140e51c748fb66502ccfc9e9033b4d96b35b8c21
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 21:46:43 +0530

    Create 1.c

---
+++ Prac set 3/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    i=n;
+    if(n==1)
+        printf("Factorial is 1");
+    else
+    {
+    for(--n;n!=1;n--)
+        i=i*n;
+    printf("Factorial is %d",i);
+    }
+    return 0;
+}
