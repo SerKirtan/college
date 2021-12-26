53109dafbf003c1e10cfc467ca1b4838931b8210
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 22:02:37 +0530

    Create 2.c

---
+++ Prac set 3/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,count=1;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    i=n;
+    printf("Reverse of %d is ",i);
+    while(i!=0)
+    {
+        n=i%10;
+        i=i/10;
+        printf("%d",n);
+    }
+    return 0;
+}
