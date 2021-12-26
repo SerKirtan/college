9ab4c4458437c65bf4b9b5095d906c69a694c446
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 09:55:00 +0530

    Create 3.c

---
+++ Prac set 1/3.c
@@ -0,0 +1,14 @@
+int main() 
+{
+ int n;
+ float p, r, SI;
+ printf("\n Enter Amount  :");
+ scanf("%f",&p);
+ printf("\n Enter No of Years :");
+ scanf("%d",&n);
+ printf("\n Enter Rate  :");
+ scanf("%f",&r);
+ SI = (p*r*n)/100;
+ printf("\n Interest = %.2f",SI);
+ return 0;
+}
