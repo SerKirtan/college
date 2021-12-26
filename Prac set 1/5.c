ac838532915b0e36cc24c52aaca9d96bb6d5b0de
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:06:38 +0530

    Create 5.c

---
+++ Prac set 1/5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d = float size\n",sizeof(float));
+    printf("%d = int size\n",sizeof(int));
+    printf("%d = double size\n",sizeof(double));
+    printf("%d = char size\n",sizeof(char));
+    return 0;
+}
