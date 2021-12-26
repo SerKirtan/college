d9596ff2a4ed82bdfc545573ba830f9137bc3456
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:25:19 +0530

    Create 5.c

---
+++ Prac set 2/5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+ float basic;
+ printf("\n Enter Basic Salary :");
+ scanf("%f",&basic);
+ printf("\n          SALARY SLIP");
+ printf("\n Basic : %.2f",basic);
+ printf("\n DA    : %.2f",basic*0.10);
+ printf("\n HRA   : %.2f",basic*0.075);
+ printf("\n MA    : %.2f",300.00);
+ printf("\n");
+ printf("\n GROSS : %.2f",basic+(basic*0.10)+(basic*0.075)+300.00);
+ printf("\n");
+ printf("\n PF    : %.2f",basic*0.125);
+ printf("\n");
+ printf("\n NET   : %.2f",(basic+(basic*0.10)+(basic*0.075)+300.00)-(basic*0.125));
+ return 0;
+}
