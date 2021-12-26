04c0e31eb38383f6af1664ed1881b0b89ea59496
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:20:31 +0530

    Create 3b.c

---
+++ Prac set 2/3b.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if(c >= 'A' && c <= 'Z' )
+        printf("'%c' is an uppercase letter.\n",c);
+
+    else if(c >= 'a' && c <= 'z')
+        printf("%c' is an lowercase letter.\n",c);
+
+    else if(c >= '0' && c <= '9')
+        printf("%c' is a Digit.\n",c);
+
+    else printf("'%c' is a Special Character.\n",c);
+
+    return 0;
+}
