68ef00f55cddae6ca8d7d411691f33606298f920
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 22:39:58 +0530

    Update 6.c

--- Prac set 3/6.c
+++ Prac set 3/6.c
@@ -1,1 +1,23 @@
 //Prime or not
+
+#include <stdio.h>
+int main()
+{
+    int n,i=2,x;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    for(;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            printf("%d is composite",n);
+            goto jump;
+        }
+    }
+    if(n==1)
+        printf("1 is neither prime nor a composite number");
+    else
+        printf("%d is prime",n);
+    jump:
+    return 0;
+}
