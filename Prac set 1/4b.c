10bff1014a2098bbac1c8e961c074d26a5cc9b32
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 09:55:41 +0530

    Create 4b.c

---
+++ Prac set 1/4b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+ int a,b;
+ printf("Enter Value of a :");
+ scanf("%d",&a);
+ printf("Enter Value of B :");
+ scanf("%d",&b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ printf("\nAfter Interchange Values A = %d B = %d",a,b);
+ return 0;
+}
