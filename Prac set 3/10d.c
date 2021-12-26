4029fa6a32267c6d6ff1e1900cdcb1ad29aa812f
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:57:32 +0530

    Create 10d.c

---
+++ Prac set 3/10d.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int r,c,rows;
+    printf("Enter rows\n");
+    scanf("%d",&rows);
+    for(r=1;r<=rows;r++)
+    {
+        for(c=1;c<=r;c++)
+        {
+            printf("%d",r);
+        }
+        printf("\n");
+    }
+    return 0;
+}
