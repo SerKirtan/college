27075bd4c46f8082b7992d7ae9a47c108cd7cc7c
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 22:48:50 +0530

    Update 7.c

--- Prac set 3/7.c
+++ Prac set 3/7.c
@@ -1,1 +1,17 @@
 // Sum of 1^2 + 2^2 + 3^2 + ... + n^2
+
+#include <stdio.h>
+int main()
+{
+    int n,i,t=1,sum=0;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        t=i*i;
+        sum=sum+t;
+    }
+    printf("Sum upto %d terms is %d",n,sum);
+    return 0;
+}
