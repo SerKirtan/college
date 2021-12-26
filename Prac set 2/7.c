17e00962ded97e14e56eef09c9e1cbe611df1732
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 11:55:12 +0530

    Create 7.c

---
+++ Prac set 2/7.c
@@ -0,0 +1,30 @@
+//Minimum and maximum of 10 numbers
+
+#include <stdio.h>
+int main()
+{
+ int a[10],i,min,max;
+ for(i=1;i<=10;i++)
+ {
+  printf("\n Enter a number [%d] : ",i);
+  scanf("%d",&a[i]);
+  if(i==0)
+  {
+   min=max=a[i];
+  }
+  else
+  {
+   if(min>a[i])
+   {
+    min=a[i];
+   }
+   if(max<a[i])
+   {
+    max=a[i];
+   }
+  }
+ }
+ printf("\n Minimum : %d",min);
+ printf("\n Maximum : %d",max);
+ return 0;
+}
