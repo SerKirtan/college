ed0c99d1c822d9fc93b2c788179feeebeb6db982
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:33:37 +0530

    Create 9c.c

---
+++ Prac set 3/9c.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int r,c,rows;
+    printf("Enter number of rows\n");
+    scanf("%d",&rows);
+    for(r=1;r<=rows;r++)
+    {
+        for(c=1;c<=2*rows-1;c++)
+        {
+            if(c<=rows-(r-1)&&c<=rows+(r-1))
+            {
+                    printf("*");
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
