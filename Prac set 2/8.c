4a4e5150ef5abc787a5efcb049b92bb44efb3b1f
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2021-12-26 10:27:20 +0530

    Create 8.c

---
+++ Prac set 2/8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+ int i,a;
+ printf("\n Enter any Number : ");
+ scanf("%d",&i);
+ a=i%10;
+ if(a%2==0)
+ {
+  printf("\n Last Digit of Number is Even");
+ }
+ else
+ {
+  printf("\n Last Digit of Number is Odd");
+ }
+ return 0;
+}
