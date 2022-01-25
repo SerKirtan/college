edecad2865d212f815f5a10c1716eedd122a216d
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-01-25 12:11:09 +0530

    Create 8.c

---
+++ Prac set 5/8.c
@@ -0,0 +1,21 @@
+//Sum of digits using recursion
+#include <stdio.h>
+int fun(int n, int rem, int sum)
+{
+    if(n==0)
+        return sum;
+    else
+    {
+        rem=n%10;
+        sum+=rem;
+        n/=10;
+        return fun(n, rem, sum);
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("Sum of digits is %d",fun(n,0,0));
+    return 0;
+}
