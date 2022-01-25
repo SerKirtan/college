db96dc32eaed96881115a868b2d17a2213189838
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-01-25 11:32:18 +0530

    Update 1.c

--- Prac set 5/1.c
+++ Prac set 5/1.c
@@ -14,3 +14,20 @@ int main()
     fun(n);
     return 0;
 }
+
+/*RECURSIVE BELOW
+#include <stdio.h>
+int fun(int n)
+{
+    if(n==1)
+        return 1;
+    else
+        return n + fun(n-1);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",fun(n));
+    return 0;
+}*/
