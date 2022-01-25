45d6831b2ebe6bab13ac4ae5d78462f32064696a
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-01-25 16:19:24 +0530

    Create 9.c

---
+++ Prac set 4/9.c
@@ -0,0 +1,34 @@
+//Replace char by another
+#include<stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[100],c1,c2;
+    int  i;
+
+    printf("Enter  the string : ");
+    gets(s);
+
+	printf("Enter a character replace: ");
+    c1=getchar();
+    getchar();
+    printf("\nEnter character to replace  with  %c : ",c1);
+    c2=getchar();
+    printf("\n\tBefore replace:%s",s);
+
+    for(i=0;s[i];i++)
+	{
+		if(s[i]==c1)
+		{
+		   s[i]=c2;
+
+	    }
+
+	}
+
+    printf("\n\tAfter replace:%s",s);
+
+
+    return 0;
+}
