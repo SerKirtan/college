7ca03ff42b00d9f584ded13d9b63e2d6ad6e662a
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-01-26 15:03:10 +0530

    Create 1.c

---
+++ Prac set 6/1.c
@@ -0,0 +1,10 @@
+//Program to print address of a variable
+#include<stdio.h>
+void main()
+{
+    int x,*ptr;
+    ptr=&x;
+    printf("Enter value of x: ");
+    scanf("%d",&x);
+    printf("Address of variable x is %d",ptr);
+}
